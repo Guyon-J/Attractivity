adc4fe9a027fd88fe431b01663f7430f10d56a43
JG <70691238+Guyon-J@users.noreply.github.com>
2026-05-26 18:40:13 +0200

    Update app.R

--- app.R
+++ app.R
@@ -2,18 +2,6 @@
 # Dépendances
 # ==========================================
 
-required_packages <- c("bslib",
-                       "dplyr",
-                       "leaflet",
-                       "shiny")
-
-new.packages <- required_packages[!(required_packages %in% installed.packages()[,"Package"])]
-if (length(new.packages)) {
-  install.packages(new.packages)
-}
-
-rm(new.packages)
-
 library(bslib)
 library(dplyr)
 library(leaflet)
